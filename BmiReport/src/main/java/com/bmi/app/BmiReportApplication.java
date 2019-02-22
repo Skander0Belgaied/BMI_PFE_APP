656d57a6bb9abffc157973438032f5d76f9b428e
zone52 <zone52@zone52-PC>
2019-02-22 14:08:17 +0100

    first push

---
+++ BmiReport/src/main/java/com/bmi/app/BmiReportApplication.java
@@ -0,0 +1,13 @@
+package com.bmi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiReportApplication.class, args);
+	}
+
+}

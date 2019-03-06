5b0438e01c4aca2aa770cb9366ad1147c46e0bed
zone52 <zone52@zone52-PC>
2019-03-06 13:59:00 +0100

    controller + front end changes

--- BmiReport/src/main/java/com/bmi/app/controllers/bmi_app/HomeController.java
+++ BmiReport/src/main/java/com/bmi/app/controllers/bmi_app/HomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bmi.app.model.bmi_app.Utilisateur;
@@ -13,20 +14,38 @@ import com.bmi.app.repository.UtilisateurRepository;
 @Controller
 public class HomeController {
 	@Autowired
-	 UtilisateurRepository U_R;
+	UtilisateurRepository U_R;
+	
+	@RequestMapping("/login")
+	String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/check")
+	String cheking() {
+		return "redirect:/";
+	}
+
 	@RequestMapping("/")
-	String home () {
-	return "index";
+	String home() {
+		return "index";
 	}
-	
-	
+
 	@RequestMapping("/sujet")
-	String sujet () {
-	return "sujet";
+	String sujet() {
+		return "sujet";
 	}
+
+	@RequestMapping(path = "/Add/Account", method = RequestMethod.GET)
+	@ResponseBody
+	String addAccount() {
+		return "work succssuffly";
+	}
+
 	@RequestMapping("/test")
 	@ResponseBody
-	List<Utilisateur> test () {
-	return U_R.findAll() ;
+	List<Utilisateur> test() {
+		return U_R.findAll();
 	}
+
 }

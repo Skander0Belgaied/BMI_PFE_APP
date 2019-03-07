b35c541f7312cb02f0976d475dc59bd665e818d1
Heni <heniabdmouleh25@gmail.com>
2019-03-07 15:00:23 +0100

    optimisation changes

--- BmiReport/src/main/java/com/bmi/app/controllers/bmi_app/TestController.java
+++ BmiReport/src/main/java/com/bmi/app/controllers/bmi_app/TestController.java
@@ -1,21 +1,13 @@
 package com.bmi.app.controllers.bmi_app;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.bmi.app.model.bmi_app.Utilisateur;
-import com.bmi.app.repository.UtilisateurRepository;
 
 @Controller
 public class TestController {
 	
 	@RequestMapping("/t")
 	String test() {
-		return "test";
+		return "update";
 	}
 }
